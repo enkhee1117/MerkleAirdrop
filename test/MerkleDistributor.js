7fee609d4b399972d65c70cf4ba29cc72ec0c2e6
enkhee1117 <enkhee1117@gmail.com>
2022-12-08 12:26:46 -0600

    added two js files that will be used for proof generation'

--- test/MerkleDistributor.js
+++ test/MerkleDistributor.js
@@ -9,20 +9,43 @@ const { ethers } = require("hardhat");
 describe("Merkledistributor", function () {
 
     async function deployLoadFixture() {
-      const rootHash = "0x05dbe3a19c155b929904a09ae9cd00a04e73334b2fdd1c00737e8a27bb5961c3";
-      const tokenAddress = ethers.utils.getAddress("0x070eb1a48725622De867a7E3d1dd4F0108966ED1");
+      const rootHash = "0xae302a9ed97fccf32a76e36fd463f56fd9c34eba17b0604dd843f74d2f4da987";
       const [owner, otherAccount] = await ethers.getSigners();
+
       const CryptoPuujin = await ethers.getContractFactory("Cryptopuujin");
       const cryptoPuujin = await CryptoPuujin.deploy();
+      console.log("CryptoPuujin deployed address: ", cryptoPuujin.address); // Delete
+      
+      const tokenAddress = cryptoPuujin.address;
+      // Deploy MerkleDistributor contract with the given token. 
       const Merkledistributor = await ethers.getContractFactory("Merkledistributor");
       const merkledistributor = await Merkledistributor.deploy(tokenAddress, rootHash);
+      const merkleContractAddress = merkledistributor.address;
+      console.log("Merkledist deployed address: ", merkledistributor.address); //Delete
 
-      return {cryptoPuujin, owner, otherAccount, rootHash, tokenAddress, merkledistributor};
+      // Add fund (1_000_000 tokens)to MerkleContract: 
+      const amountToSend = ethers.BigNumber.from("1000000000000000000000");
+      await cryptoPuujin.mint(merkleContractAddress, amountToSend);
+
+      return {cryptoPuujin, merkleContractAddress, owner};
     }
 
     describe("Deployment", function(){
+
       it("Should deploy the contract with given parameters", async function (){
           await loadFixture(deployLoadFixture);
       })
-    });
+
+      it("Should have the balance of 1_000_000 in merkle contract", async function (){
+        let balanceExpected = ethers.BigNumber.from("1000000000000000000000");
+        const {cryptoPuujin, merkleContractAddress, owner} = await loadFixture(deployLoadFixture);
+        expect(await cryptoPuujin.balanceOf(merkleContractAddress)).to.equal(balanceExpected);
+      })
+
+      it("Should be able to claim from address 0x1111111111111111111111111111111111111111 amount 1000000000000000000", async function() {
+        
+      })
+
+    }); //END OF DESCRIBE
 })
+
